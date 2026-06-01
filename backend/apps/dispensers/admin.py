34c6c98f10e300fdff9c2f3b66dc86742c4aac4e
raycuba <raycubA15@gmail.com>
2026-06-01 10:46:14 +0200

    permitiendo adinistracion en db

--- backend/apps/dispensers/admin.py
+++ backend/apps/dispensers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Dispenser, DispenserUsage
+
+@admin.register(Dispenser)
+class DispenserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'flow_volume', 'status',)
+    search_fields = ('status',)
+    
+
+@admin.register(DispenserUsage)
+class DispenserUsageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'dispenser', 'opened_at', 'closed_at', 'flow_volume')
+    search_fields = ('dispenser',)
